9d7f80b1d21812c3a35bbecd3c7a037317324508
dncoder14 <dhirajpandit5050@gmail.com>
2024-12-01 16:41:28 +0530

    code of our e-commerce website

---
+++ scriptmen.js
@@ -0,0 +1,6 @@
+// Add interactivity to your site
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    alert("Redirecting to product page...");
+  });
+});
